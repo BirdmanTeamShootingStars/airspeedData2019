b2d533367e65b0ff2c41e33e782eaaef8696d86a
yukiTakezawa <courage.t0629@gmail.com>
2019-06-22 18:01:19 +0900

    fix

--- regression.py
+++ regression.py
@@ -2,6 +2,8 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
+from math import sqrt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("csv_filename")
@@ -12,7 +14,7 @@ X = np.array([data['v'].values]).T
 y = np.array(data['p'].values)
 
 for i in range(y.size):
-    y[i] = y[i]**2
+    y[i] = sqrt(y[i])
 
 print("X shape: {}" .format(X.shape))
 print("y shape: {}" .format(y.shape))
@@ -20,3 +22,6 @@ model = LinearRegression().fit(X, y)
 print("train score: {}" .format(model.score(X,y)))
 print("coef_: {}" .format(model.coef_))
 print("intercept_: {}" .format(model.intercept_))
+
+plt.plot(X, y, 'o')
+plt.show()
