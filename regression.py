3efc3b3fd5b1d76a92501e61dda00fb016771cf5
yukiTakezawa <courage.t0629@gmail.com>
2019-06-25 15:24:59 +0900

    fix

--- regression.py
+++ regression.py
@@ -10,11 +10,11 @@ parser.add_argument("csv_filename")
 args = parser.parse_args()
 
 data = pd.read_csv(args.csv_filename)
-X = np.array([data['v'].values]).T
-y = np.array(data['p'].values)
+y = np.array(data['v'].values)
+X = np.array([data['p'].values]).T
 
-for i in range(y.size):
-    y[i] = sqrt(y[i])
+for i in range(X.size):
+    X[i] = sqrt(X[i])
 
 print("X shape: {}" .format(X.shape))
 print("y shape: {}" .format(y.shape))
@@ -23,5 +23,14 @@ print("train score: {}" .format(model.score(X,y)))
 print("coef_: {}" .format(model.coef_))
 print("intercept_: {}" .format(model.intercept_))
 
+x_domain = np.array([np.linspace(0.15, 0.4)]).T
+for i in range(x_domain.size):
+    x_domain[i] = sqrt(x_domain[i])
+
+y_predict = model.predict(x_domain)
+
 plt.plot(X, y, 'o')
+plt.plot(x_domain, y_predict, 'x')
+plt.xlabel('sqrt(p)')
+plt.ylabel('v')
 plt.show()
